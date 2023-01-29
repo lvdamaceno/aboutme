a05cab1fa7e10e46dbcb651e032d1606d992874b
lvdamaceno <lvdamaceno@gmail.com>
2023-01-29 10:37:01 -0300

    profile pic address correction

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function toggleMode() {
   // Change the profile pic
   const img = document.querySelector("#profile img")
   if (html.classList.contains('light')) {
-    img.setAttribute('src', '/assets/img/profile/profile_light.png')
+    img.setAttribute('src', './assets/img/profile/profile_light.png')
   } else {
-    img.setAttribute('src', '/assets/img/profile/profile_dark.png')
+    img.setAttribute('src', './assets/img/profile/profile_dark.png')
   }
 
 }
